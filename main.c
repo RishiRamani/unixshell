44972426eafc492ed2bc6fb56edc196c2195c1e3
Rishi Ramani <rishiramani1512@gmail.com>
2026-03-26 14:43:46 +0000

    Added pipelining

--- main.c
+++ main.c
@@ -6,6 +6,14 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
+
+/*
+pipe - detect pipe
+ fork twice 
+ create pipe btw both
+ do dup2 to redfine fd
+ exec both
+*/
 //Tokenization
 int tokenize(char *input,char *tokens[]);
 //Handler
@@ -14,11 +22,15 @@ int handle_builtin(char *tokens[],int token_count);
 void handle_redirection(char *tokens[],int token_count);
 //Execution
 void execute_command(char *tokens[],int token_count);
-
+//Pipe Split
+int split_commands(char *tokens[],int token_count,char *cmds[][100]);
+//Pipeline execution
+void execute_pipeline_commands(char *cmds[][100],int cmd_count);
 
 int main(void){
   char input[1024];
-  char * tokens[100]; // Array to hold pointers to tokens 
+  char *tokens[100]; // Array to hold pointers to tokens 
+  char *cmds[10][100];
 
   while(1){
 
@@ -40,8 +52,14 @@ int main(void){
     if(handle_builtin(tokens,token_count)){
       continue;
     }
-
-    execute_command(tokens,token_count);
+    int cmd_count = split_commands(tokens,token_count,cmds);
+    if(cmd_count>1){
+      execute_pipeline_commands(cmds,cmd_count);
+    }else{
+      execute_command(tokens,token_count);
+    }
+    
+    
   }
   return 0;
 }
@@ -174,5 +192,72 @@ void execute_command(char *tokens[],int token_count){
   } else {
     // Parent process
     wait(NULL); // Wait for the child process to finish
+  }
+}
+
+int split_commands(char *tokens[],int token_count,char *cmds[][100]){
+  int cmd_index=0;
+  int arg_index=0;
+  for(int i=0;i<token_count;i++){
+    if(strcmp(tokens[i],"|")==0){
+      cmds[cmd_index++][arg_index]=NULL;
+      if(cmd_index>10){
+        fprintf(stderr, "Too many commands\n");
+        exit(1);
+      }
+      arg_index=0;
+    }else{
+      cmds[cmd_index][arg_index++]=tokens[i];
+    }
+  }
+  cmds[cmd_index][arg_index]=NULL;
+  return cmd_index+1;
+}
+
+void execute_pipeline_commands(char *cmds[][100],int cmd_count){
+  int pipes[cmd_count-1][2];
+  for(int i=0;i<cmd_count-1;i++){
+    if(pipe(pipes[i])<0){
+      perror("Pipe failed");
+      exit(1);
+    }
+  
+  }
+  for(int i=0;i<cmd_count;i++){
+    pid_t pid=fork();
+    if(pid < 0){
+      perror("Fork failed");
+      exit(EXIT_FAILURE);
+    }else if(pid == 0){
+      if(i==0){
+        dup2(pipes[i][1],STDOUT_FILENO);
+      }
+      else if(i>0 && i<cmd_count-1){
+        dup2(pipes[i-1][0],STDIN_FILENO);
+        dup2(pipes[i][1],STDOUT_FILENO);
+      }else if(i==cmd_count-1){
+        dup2(pipes[i-1][0],STDIN_FILENO);
+      }
+      for(int j = 0; j < cmd_count - 1; j++){
+        close(pipes[j][0]);
+        close(pipes[j][1]);
+      }
+      int cmd_arg_count = 0;
+      while(cmds[i][cmd_arg_count] != NULL){
+        cmd_arg_count++;
+      }
+      handle_redirection(cmds[i],cmd_arg_count);
+      execvp(cmds[i][0],cmds[i]);
+      perror("exec failed");
+      exit(1);
+    }
+
+  }
+  for(int i=0;i<cmd_count-1;i++){
+    close(pipes[i][0]);
+    close(pipes[i][1]);
+  }
+  for(int i=0;i<cmd_count;i++){
+    wait(NULL);
   }
 }
